fe9db597b233376c6e70aa17e2e433c872172fbe
jeepkid <jeepkid@163.com>
2016-02-29 15:08:54 +0800

    fix

---
+++ src/dynamodbproc.py
@@ -0,0 +1,50 @@
+#!usr/bin/python
+# coding=utf-8
+import boto3
+import json
+from boto3.dynamodb.conditions import Key, Attr
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def InitDyDB(Tname):
+    dydb = boto3.resource('dynamodb')
+    dytb = dydb.Table(Tname)
+    return dytb
+
+def DyDBClient():
+    client = boto3.client('dynamodb')
+    paginator = client.get_paginator('scan')
+    response_iterator = paginator.paginate(
+        Select = 'SPECIFIC_ATTRIBUTES',
+        TableName='Tweestatus',
+        AttributesToGet=[
+            'id',
+            'text',
+            'userid'
+
+            # 'coordinates'
+        ]
+        # ProjectionExpression='text,coordinates'
+    )
+    ucount = 0
+    fp = open('./outputstatus','w+r')
+    for itemiterator in response_iterator:
+        for item in itemiterator['Items']:
+            ucount += 1
+            dict = {'id':item['id']['N'],'text':item['text']['S'],'userid':item['userid']['N']}
+            fp.writelines(json.dumps(dict,ensure_ascii=False,indent=4))
+    print ucount
+def exportstatus(tb):
+    response = tb.scan(
+
+        FilterExpression=Attr('id').gt(0)
+    )
+    for item in response['Items']:
+        print item
+    # items = response['Items']
+
+# tb = InitDyDB('Tweestatus')
+# exportstatus(tb)
+DyDBClient()
--- src/tweecore.py
+++ src/tweecore.py
@@ -1,5 +1,5 @@
 #!usr/bin/python
-#coding=utf-8
+# coding=utf-8
 
 import tweepy
 import json
@@ -7,234 +7,246 @@ import time
 
 __DEBUG__ = False
 
-class DoAuth(object):
-	"""docstring for ClassName"""
-	def __init__(self):
-		self.APIKey = "yZ2IIZLtwRzgrUwuhJxAFGYxh"
-		self.APISecret = "e33QxsLEDkXBzVMzJJP6bGgQUGiPR0l2xFbV6LYr5Rm4NU4cq3"
-		self.AccessToken = "4905001899-rYjepLfO8IdIMkSBrrwXJs6Wbd4Eo6S9eMjshYR"
-		self.AccessTokenSecret = "AMH3YGv5jPc2I5kXw2Dq3Rchios8z78IErvbSMwCIyteA"
-	def doauth(self):
-		auth = tweepy.OAuthHandler(self.APIKey,self.APISecret)
-		auth.set_access_token(self.AccessToken,self.AccessTokenSecret)
-		return auth
-	def doapi(self,arg):
-		return tweepy.API(arg)
 
-def retry(times=1,sleep=10,exceptions=None):
-	import time
-	exceptions = exceptions if exceptions is not None else Exception
-	def wrapper(func):
-		def wrapper(*args,**kwargs):
-			last_exception =None
-			for ct in range(times):
-				try:
-					return func(*args, **kwargs)
-				except exceptions as e:
-					last_exception = e
-					print "[Exception]: ",last_exception
-					print "[Retrytimes]: " , ct
-					time.sleep(sleep)
-			raise last_exception
-		return wrapper
-	return wrapper
+class DoAuth(object):
+    """docstring for ClassName"""
+
+    def __init__(self):
+        self.APIKey = "yZ2IIZLtwRzgrUwuhJxAFGYxh"
+        self.APISecret = "e33QxsLEDkXBzVMzJJP6bGgQUGiPR0l2xFbV6LYr5Rm4NU4cq3"
+        self.AccessToken = "4905001899-rYjepLfO8IdIMkSBrrwXJs6Wbd4Eo6S9eMjshYR"
+        self.AccessTokenSecret = "AMH3YGv5jPc2I5kXw2Dq3Rchios8z78IErvbSMwCIyteA"
+
+    def doauth(self):
+        auth = tweepy.OAuthHandler(self.APIKey, self.APISecret)
+        auth.set_access_token(self.AccessToken, self.AccessTokenSecret)
+        return auth
+
+    def doapi(self, arg):
+        return tweepy.API(arg)
+
+
+def retry(times=1, sleep=10, exceptions=None):
+    import time
+    exceptions = exceptions if exceptions is not None else Exception
+
+    def wrapper(func):
+        def wrapper(*args, **kwargs):
+            last_exception = None
+            for ct in range(times):
+                try:
+                    return func(*args, **kwargs)
+                except exceptions as e:
+                    last_exception = e
+                    print "[Exception]: ", last_exception
+                    print "[Retrytimes]: ", ct
+                    time.sleep(sleep)
+            raise last_exception
+
+        return wrapper
+
+    return wrapper
 
 
 def CallRatelimit(func):
-	def _deco(s,x):
-		s60s = ["process_follow","process_friend"]
-		s30s = ["process_status"]
-		ret = func(s,x)
-		if __DEBUG__:
-			time.sleep(1)
-		else:
-			if func.func_name in s30s:
-				time.sleep(30)
-			else:
-				time.sleep(60)
-		return ret
-	return _deco
+    def _deco(s, x):
+        s60s = ["process_follow", "process_friend"]
+        s30s = ["process_status"]
+        ret = func(s, x)
+        if __DEBUG__:
+            time.sleep(1)
+        else:
+            if func.func_name in s30s:
+                time.sleep(30)
+            else:
+                time.sleep(60)
+        return ret
+
+    return _deco
 
 
 class UserObj():
-	def __init__(self,twapi,uid,uname=None):
-		self.uid = uid
-		self.uname = uname
-		self.twapi = twapi
-		self.followersidlst = []
-		self.friendsid = []
-		self.relKey = "id"
-		self.FOLLOW = 1
-		self.FRIEND = 2
+    def __init__(self, twapi, uid, uname=None):
+        self.uid = uid
+        self.uname = uname
+        self.twapi = twapi
+        self.followersidlst = []
+        self.friendsid = []
+        self.relKey = "id"
+        self.FOLLOW = 1
+        self.FRIEND = 2
 
-	@retry(1000)
-	def get_user_info(self):
-		if self.uname == None:
-			self.userobj = self.twapi.get_user(self.uid)
-		else:
-			self.userobj = self.twapi.get_user(self.uname)
-		return self._processret(self.userobj)
+    @retry(1000)
+    def get_user_info(self):
+        if self.uname == None:
+            self.userobj = self.twapi.get_user(self.uid)
+        else:
+            self.userobj = self.twapi.get_user(self.uname)
+        return self._processret(self.userobj)
 
-	@retry(1000)
-	def get_followers_page(self):
-		for page in tweepy.Cursor(self.twapi.followers,id=self.uid).pages():
-		# return self.api.followers()
-			yield self.process_follow(page)
-	
-	@retry(1000)
-	def get_friends_page(self):
-		for page in tweepy.Cursor(self.twapi.friends,id=self.uid).pages():
-			yield self.process_friend(page)
+    @retry(1000)
+    def get_followers_page(self):
+        for page in tweepy.Cursor(self.twapi.followers, id=self.uid).pages():
+            # return self.api.followers()
+            yield self.process_follow(page)
 
-	@CallRatelimit
-	def process_follow(self,pagedata):
-		return	self.processret(pagedata,self.FOLLOW)
+    @retry(1000)
+    def get_friends_page(self):
+        for page in tweepy.Cursor(self.twapi.friends, id=self.uid).pages():
+            yield self.process_friend(page)
 
-	@CallRatelimit
-	def process_friend(self,pagedata):
-		return  self.processret(pagedata,self.FRIEND)
-	
-	def processret(self,items,source):
-		resultlst = [self._processret(x) for x in items]
-		self._append_lst(source,resultlst)
-		return resultlst
+    @CallRatelimit
+    def process_follow(self, pagedata):
+        return self.processret(pagedata, self.FOLLOW)
 
-	def _processret(self,item):
-		ret = {}
-		# for key in item.__dict__:
-			# print type(item.__dict__[key])
-			# print key," : ",item.__dict__[key],"\n\n\n\n"
-			# ret[key] = item.__dict__[key]
-			# self.followersidlst.append(item.__dict__["id"])	
-		# return ret
-		return item._json
-		# return json.loads(json.dumps(item._json))
+    @CallRatelimit
+    def process_friend(self, pagedata):
+        return self.processret(pagedata, self.FRIEND)
 
-	def _append_lst(self,source,slst):
-		if source == self.FOLLOW: #follow
-			self.followersidlst += self._mk_lst(slst)
-		elif source == self.FRIEND: #friend
-			self.friendsid += self._mk_lst(slst)
+    def processret(self, items, source):
+        resultlst = [self._processret(x) for x in items]
+        self._append_lst(source, resultlst)
+        return resultlst
 
-	def _mk_lst(self,slst):
-		return [item[self.relKey] for item in slst]
+    def _processret(self, item):
+        ret = {}
+        # for key in item.__dict__:
+        # print type(item.__dict__[key])
+        # print key," : ",item.__dict__[key],"\n\n\n\n"
+        # ret[key] = item.__dict__[key]
+        # self.followersidlst.append(item.__dict__["id"])
+        # return ret
+        return item._json
 
-	def show_relids(self,tag):
-		if tag == 1:
-			return self.followersidlst
-		elif tag == 2:
-			return self.friendsid
-		elif tag == 0:
-			return {"id":self.uid,"follows":self.followersidlst,"friends":self.friendsid}
+    # return json.loads(json.dumps(item._json))
+
+    def _append_lst(self, source, slst):
+        if source == self.FOLLOW:  # follow
+            self.followersidlst += self._mk_lst(slst)
+        elif source == self.FRIEND:  # friend
+            self.friendsid += self._mk_lst(slst)
+
+    def _mk_lst(self, slst):
+        return [item[self.relKey] for item in slst]
+
+    def show_relids(self, tag):
+        if tag == 1:
+            return self.followersidlst
+        elif tag == 2:
+            return self.friendsid
+        elif tag == 0:
+            return {"id": self.uid, "follows": self.followersidlst, "friends": self.friendsid}
 
 
 class SearchUser():
-	def __init__(self):
-		pass
-	def search_by_name(self):
-		pass
+    def __init__(self):
+        pass
+
+    def search_by_name(self):
+        pass
+
 
 class Status():
-	def __init__(self,twapi,uid):
-		self.twapi = twapi
-		self.uid = uid
+    def __init__(self, twapi, uid):
+        self.twapi = twapi
+        self.uid = uid
 
-	def usr_timeline(self):
-		self.twobj = self.twapi.user_timeline(self.uid)
-		return self.twobj
-	
-	@retry(1000)
-	def get_status_page(self):
-		for page in tweepy.Cursor(self.twapi.user_timeline,id=self.uid).pages():
-			yield self.process_status(page)
+    def usr_timeline(self):
+        self.twobj = self.twapi.user_timeline(self.uid)
+        return self.twobj
 
-	@CallRatelimit
-	def process_status(self,pagedata):
-		resultlst = [self._processret(item) for item in pagedata]
-		return resultlst
+    @retry(1000)
+    def get_status_page(self):
+        for page in tweepy.Cursor(self.twapi.user_timeline, id=self.uid).pages():
+            yield self.process_status(page)
 
-	# def processret(self,pagedata):
-	# 	pass
+    @CallRatelimit
+    def process_status(self, pagedata):
+        resultlst = [self._processret(item) for item in pagedata]
+        return resultlst
 
-	def _processret(self,item):
-		ret = {}
-		# for key in item.__dict__:
-		# 	ret[key] = item.__dict__[key]
-		# return ret
-		# %a %b %c %Y
-		return item._json
+    # def processret(self,pagedata):
+    # 	pass
+
+    def _processret(self, item):
+        ret = {}
+        # for key in item.__dict__:
+        # 	ret[key] = item.__dict__[key]
+        # return ret
+        # %a %b %c %Y
+        return item._json
+
 
 ######    test...
 if __name__ == '__main__':
+    # test
+    doauth = DoAuth()
+    auth = doauth.doauth()
+    api = doauth.doapi(auth)
 
-	# test
-	doauth = DoAuth()
-	auth = doauth.doauth()
-	api = doauth.doapi(auth)
-
-	# def testUserObj():
-	# 	user = UserObj(api,2875746901)
-	# 	for page in user.get_followers_page():
-	# 		print "One page followers len:",len(page),"\n\n\n\n\n"
-
-
-	# 	for page in user.get_friends_page():
-	# 		print "One page friends len:",len(page),"\n\n\n\n\n"
-
-	# 	print user.show_relids(0)
-	# 	print "follows len:",len(user.show_relids(1)),"  friends len:",len(user.show_relids(2))
-	# def testStatus():
-	# 	status = Status(api,2875746901)
-	# 	create_dtlst = []
-	# 	for page in status.get_status_page():
-	# 		print "On Page status len :",len(page)
-	# 		for item in page:
-	# 			create_dtlst.append(item["created_at"])
-	# 	print create_dtlst
-	# testStatus()
-
-	### old way
-	# page = 1
-	# while True:
-	# 	statuses = api.user_timeline(page=page)
-	# 	if statuses:
-	# 		for status in statuses:
-	# 			print status
-	# 	else:
-	# 		break
-	# 	page += 1 
+    # def testUserObj():
+    # 	user = UserObj(api,2875746901)
+    # 	for page in user.get_followers_page():
+    # 		print "One page followers len:",len(page),"\n\n\n\n\n"
 
 
-	# ret = api.followers(id=203829129)
-	# ret = ret[0]
-	# print ret,"\n\n\n\n\n\n\n\n"
+    # 	for page in user.get_friends_page():
+    # 		print "One page friends len:",len(page),"\n\n\n\n\n"
 
-	# print dir(ret)
-	# print type(ret),"\n\n\n\n\n\n\n\n"
+    # 	print user.show_relids(0)
+    # 	print "follows len:",len(user.show_relids(1)),"  friends len:",len(user.show_relids(2))
+    # def testStatus():
+    # 	status = Status(api,2875746901)
+    # 	create_dtlst = []
+    # 	for page in status.get_status_page():
+    # 		print "On Page status len :",len(page)
+    # 		for item in page:
+    # 			create_dtlst.append(item["created_at"])
+    # 	print create_dtlst
+    # testStatus()
 
-	# for key in ret.__dict__:
-	# 	print key
+    ### old way
+    # page = 1
+    # while True:
+    # 	statuses = api.user_timeline(page=page)
+    # 	if statuses:
+    # 		for status in statuses:
+    # 			print status
+    # 	else:
+    # 		break
+    # 	page += 1
 
-	
-	###### new cursor way
-	# for status in tweepy.Cursor(api.user_timeline).items(): #pages() #items(300)  #pages(3)
-	# 	print status
 
-	# for item in tweepy.Cursor(api.followers,id=203829129).pages():
-	# 	print item
-	# 	time.sleep(10)
+    # ret = api.followers(id=203829129)
+    # ret = ret[0]
+    # print ret,"\n\n\n\n\n\n\n\n"
 
-	# loop = 0
-	# for page in tweepy.Cursor(api.followers,id=203829129,page=18).pages():
-	# 	time.sleep(10)
-	# 	loop += 1
-	# 	print page
-	# 	print "loop:",loop
+    # print dir(ret)
+    # print type(ret),"\n\n\n\n\n\n\n\n"
 
-	# public_tweets = api.home_timeline()
-	# for item in public_tweets:
-	# 	print item
-	uobj = UserObj(api,uid = 203829129)
-	uobj.get_user_info()
+    # for key in ret.__dict__:
+    # 	print key
 
-# api.xxx()
+
+    ###### new cursor way
+    # for status in tweepy.Cursor(api.user_timeline).items(): #pages() #items(300)  #pages(3)
+    # 	print status
+
+    # for item in tweepy.Cursor(api.followers,id=203829129).pages():
+    # 	print item
+    # 	time.sleep(10)
+
+    # loop = 0
+    # for page in tweepy.Cursor(api.followers,id=203829129,page=18).pages():
+    # 	time.sleep(10)
+    # 	loop += 1
+    # 	print page
+    # 	print "loop:",loop
+
+    # public_tweets = api.home_timeline()
+    # for item in public_tweets:
+    # 	print item
+    uobj = UserObj(api, uid=203829129)
+    uobj.get_user_info()
+
+    # api.xxx()
